a1c1c71a68f849c95c899361a5eed1aa0b9b0baa
Blondy Martinez <blondymartinezm@gmail.com>
2024-07-16 12:04:31 +0200

    responsive home and added extra margin to title

--- src/javascript/views/home.js
+++ src/javascript/views/home.js
@@ -12,12 +12,12 @@ const Home = () => {
         <div className='mt-0'>
             <ThreeScene />
             <div className='content bg-transparent d-flex flex-column align-items-center justify-content-center mt-5'>
-                <img src={title} className='w-30 mb-4'></img>
+                <img src={title} className='w-30 mb-3 mt'></img>
                 <div className='d-flex justify-content-between w-30'>
                   <FancyButton icon={'mingcute:code-line'} text={'Projects'} handleClick={() => navigate('/projects')}/>
                   <div className='d-flex gap-2'>
-                    <FancyButton icon={'mingcute:github-line'} handleClick={() => window.open('https://github.com/BlondyMartinez', '_blank')}></FancyButton>
-                    <FancyButton icon={'ri:linkedin-line'} handleClick={() => window.open('https://www.linkedin.com/in/blondy-martinez/', '_blank')}></FancyButton>
+                      <FancyButton icon={'mingcute:github-line'} handleClick={() => window.open('https://github.com/BlondyMartinez', '_blank')}></FancyButton>
+                      <FancyButton icon={'ri:linkedin-line'} handleClick={() => window.open('https://www.linkedin.com/in/blondy-martinez/', '_blank')}></FancyButton>
                   </div>
                 </div>
             </div>
