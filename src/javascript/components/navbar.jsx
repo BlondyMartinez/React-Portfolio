ead560353ff5b86581474f3973e983e957c1dae2
Blondy Martinez <blondymartinezm@gmail.com>
2024-07-17 04:12:48 +0200

    fixed navbar and now links are react router links

--- src/javascript/components/navbar.jsx
+++ src/javascript/components/navbar.jsx
@@ -2,20 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../styles/navbar.css';
 
 const PortofolioNavbar = () => {
     return (
         <>
-            <Navbar className="bg-body-tertiary fixed-top bg-transparent">
+            <Navbar collapseOnSelect expand='lg' variant="dark" className="bg-body-tertiary fixed-top bg-transparent">
                 <Container fluid>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" className="ms-auto" />
                     <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
                         <Nav>
-                            <Nav.Link href="#about-me" className="text-white">About Me</Nav.Link>
-                            <Nav.Link href="#projects" className="text-white">Projects</Nav.Link>
-                            <Nav.Link href="#experience" className="text-white">Experience</Nav.Link>
-                            <Nav.Link href="#contact-me" className="text-white">Contact Me</Nav.Link>
+                            <Nav.Link as={Link} to="/about" className="text-white">About Me</Nav.Link>
+                            <Nav.Link as={Link} to="/projects" className="text-white">Projects</Nav.Link>
+                            <Nav.Link as={Link} to="/experience" className="text-white">Experience</Nav.Link>
+                            <Nav.Link as={Link} to="/contact-me" className="text-white">Contact Me</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
