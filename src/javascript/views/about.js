6ebfed69a20859c5a11f8c01594e6c9ad34a8c7b
Blondy Martinez <blondymartinezm@gmail.com>
2024-07-18 04:01:15 +0200

    added astronaut with my face to about view

---
+++ src/javascript/views/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Astronaut from "../components/astronaut.jsx";
+
+function About() {
+    return (
+        <div className="p-0">
+            <Astronaut></Astronaut>
+        </div>
+    );
+}
+  
+export default About;
+  
