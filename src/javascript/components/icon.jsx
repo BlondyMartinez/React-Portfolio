efc92efc031c1781b67ac8e69a7cbfa804fbd58c
Blondy Martinez <blondymartinezm@gmail.com>
2024-08-04 02:44:33 +0200

    added extra svg

--- src/javascript/components/icon.jsx
+++ src/javascript/components/icon.jsx
@@ -4,6 +4,18 @@ import { ReactComponent as CPP } from '../../svg/c.svg';
 import { ReactComponent as JS } from '../../svg/javascript-svgrepo-com.svg';
 import { ReactComponent as PY } from '../../svg/python-svgrepo-com.svg';
 import { ReactComponent as RUBY } from '../../svg/ruby-svgrepo-com.svg';
+import { ReactComponent as CSS } from '../../svg/css-3.svg';
+import { ReactComponent as HTML } from '../../svg/html-1.svg';
+import { ReactComponent as GIT } from '../../svg/git-icon.svg';
+import { ReactComponent as FLASK } from '../../svg/flask.svg';
+import { ReactComponent as JEST } from '../../svg/jest-2.svg';
+import { ReactComponent as PS } from '../../svg/photoshop-cc-4.svg';
+import { ReactComponent as RSPEC } from '../../svg/RSpec.svg';
+import { ReactComponent as SQLA } from '../../svg/SQLAlchemy.svg';
+import { ReactComponent as UNITY } from '../../svg/Unity.svg';
+import { ReactComponent as UE } from '../../svg/Unreal Engine.svg';
+import { ReactComponent as REACT } from '../../svg/react-2.svg';
+
 import "../../styles/skill.css"
 
 const SVGIcon = ({ iconName }) => {
@@ -12,7 +24,18 @@ const SVGIcon = ({ iconName }) => {
         cpp: CPP,
         js: JS,
         py: PY,
-        ruby: RUBY
+        ruby: RUBY,
+        css: CSS,
+        html: HTML,
+        git: GIT,
+        flask: FLASK,
+        jest: JEST,
+        ps: PS,
+        rspec: RSPEC,
+        sqla: SQLA,
+        unity: UNITY,
+        ue: UE,
+        react: REACT
     };
     
     const IconComponent = svgMap[iconName];
