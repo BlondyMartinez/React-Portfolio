3d6798487210992861ef508fd8a785ea94511bc5
Blondy Martinez <blondymartinezm@gmail.com>
2024-08-05 19:34:03 +0200

    if small device, projects button text shorter

--- src/javascript/views/home.js
+++ src/javascript/views/home.js
@@ -3,15 +3,17 @@ import "../../styles/home.css"
 import title from "../../img/title.png"
 import FancyButton from '../components/button.jsx';
 import { useNavigate } from 'react-router-dom';
+import useScreenWidth from '../hooks/useScreenWidth.jsx';
 
 const Home = () => {
     const navigate = useNavigate();
+    const smallDevice = useScreenWidth();
 
     return (
         <div className='d-flex flex-column align-items-center justify-content-center mt'>
             <img src={title} className='w-30 mb-3 opacity-80'></img>
             <div className='d-flex justify-content-between w-30'>
-                <FancyButton icon={'mingcute:code-line'} text={'Discover My Projects'} handleClick={() => navigate('/projects')}/>
+                <FancyButton icon={'mingcute:code-line'} text={smallDevice ? 'Projects' : 'Discover My Projects'} handleClick={() => navigate('/projects')}/>
                    <div className='d-flex gap-2'>
                     <FancyButton icon={'mingcute:github-line'} handleClick={() => window.open('https://github.com/BlondyMartinez', '_blank')}></FancyButton>
                     <FancyButton icon={'ri:linkedin-line'} handleClick={() => window.open('https://www.linkedin.com/in/blondy-martinez/', '_blank')}></FancyButton>
