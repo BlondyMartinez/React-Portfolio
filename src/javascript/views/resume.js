70a50ee88db70f91525a42f23b7f907dce2fbe43
Blondy Martinez <blondymartinezm@gmail.com>
2024-08-06 04:50:34 +0200

    height fix

--- src/javascript/views/resume.js
+++ src/javascript/views/resume.js
@@ -89,7 +89,7 @@ const Resume = () => {
     ]
 
     return (
-        <div className='h-100 d-flex flex-column justify-content-between align-items-center'>
+        <div className='min-height d-flex flex-column justify-content-between align-items-center'>
             <div className='mt-5'>
                 <a className="button d-inline-flex align-items-center gap-2 text-decoration-none" href="https://raw.githubusercontent.com/BlondyMartinez/React-Portfolio/master/src/pdf/Resume_BlondyMartinez.pdf" download>
                     <Icon className='fs-3' icon='material-symbols-light:download' /> Download Resume
