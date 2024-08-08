31572e5db3e80ebcdebe909e3e71df543ca83fe8
Blondy Martinez <blondymartinezm@gmail.com>
2024-08-09 00:42:54 +0200

    styled scrollbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import './styles/scrollbar.css';
 import reportWebVitals from './tests/reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './Layout';
