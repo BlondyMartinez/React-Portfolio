5ac543c2c0442a7ac5dd5f653eac635c2790e65a
Blondy Martinez <blondymartinezm@gmail.com>
2024-08-08 23:48:46 +0200

    restyled code snippet component

--- src/javascript/components/code-highlighter.jsx
+++ src/javascript/components/code-highlighter.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/tomorrow-night-bright.css'; 
-import '../../styles/code.css'
 
 const CodeHighlighter = ({ code, language }) => {
     const codeRef = useRef(null);
@@ -13,8 +12,8 @@ const CodeHighlighter = ({ code, language }) => {
     }, [code, language]);
 
     return (
-        <pre className='small-code'>
-            <code ref={codeRef} className={`language-${language}`}>
+        <pre className='small-code w-100 m-0'>
+            <code ref={codeRef} className={`language-${language} radius-20`}>
                 {code}
             </code>
         </pre>
