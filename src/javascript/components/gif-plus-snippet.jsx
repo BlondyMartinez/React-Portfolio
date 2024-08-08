066481278a2b558726ad07d4b3d00aca468fc7e1
Blondy Martinez <blondymartinezm@gmail.com>
2024-08-09 00:43:54 +0200

    added another prop to be more flexible with size

--- src/javascript/components/gif-plus-snippet.jsx
+++ src/javascript/components/gif-plus-snippet.jsx
@@ -3,11 +3,11 @@ import { Row, Col } from 'react-bootstrap';
 import CodeHighlighter from "./code-highlighter.jsx";
 import '../../styles/gif-plus-snippet.css'
 
-const ClipPlusSnippet = ({ gif, snippet, language, order }) => {
+const ClipPlusSnippet = ({ gif, snippet, language, order, boids }) => {
     return (
         <Row className="d-flex justify-content-center">
             <Col className="m-auto" sm={11} lg={'auto'}>
-                <img src={gif} className={`gif ${order == 'first' ? '' : 'float-end'}`} ></img>
+                <img src={gif} className={`${order == 'first' ? '' : 'float-end'} ${boids ? 'boids' : 'gif'}`} ></img>
             </Col>
             <Col xs={{ order: order }} className="m-auto">
                 <CodeHighlighter code={snippet} language={language} />
