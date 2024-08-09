4d2e4344196b18f72d6672a8888988fdd2fdf01d
Blondy Martinez <blondymartinezm@gmail.com>
2024-08-09 02:54:51 +0200

    removed unused imports

--- src/javascript/components/image-zoom.jsx
+++ src/javascript/components/image-zoom.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/image-zoom.css'; 
 import useScreenWidth from '../hooks/useScreenWidth';
